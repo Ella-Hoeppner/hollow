72117b8d456b967f434d2973317b2b2c8cfbc3b0
Ella Hoeppner <ellahoeppner@gmail.com>
2022-09-04 12:54:48 -0400

    Fixing broken ref in dev/startup

--- src/sprog/dev/startup.cljs
+++ src/sprog/dev/startup.cljs
@@ -1,5 +1,5 @@
 (ns sprog.dev.startup
-  (:require [sprog.dev.fn-sort-demo
+  (:require [sprog.dev.basic-demo
              :rename
              {init init-basic-demo}]
             [sprog.dev.multi-texture-output-demo
@@ -10,9 +10,9 @@
              {init init-fn-sort-demo}]))
 
 (defn init []
-  #_(init-basic-demo)
+  (init-basic-demo)
   #_(init-multi-texture-output-demo)
-  (init-fn-sort-demo))
+  #_(init-fn-sort-demo))
 
 (defn pre-init []
   (js/window.addEventListener "load" (fn [_] (init))))
