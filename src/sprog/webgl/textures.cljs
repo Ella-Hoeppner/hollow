d705445dd53bd1a6808ff182fb2daa37336edbf4
Ella Hoeppner <ellahoeppner@gmail.com>
2022-09-04 14:19:28 -0400

    Adding helper fn for loading html images

--- src/sprog/webgl/textures.cljs
+++ src/sprog/webgl/textures.cljs
@@ -1,5 +1,30 @@
 (ns sprog.webgl.textures)
 
+(defn set-texture-parameters [gl texture filter-mode wrap-mode]
+  (.bindTexture gl gl.TEXTURE_2D texture)
+  (let [gl-filter-mode ({:linear gl.LINEAR
+                         :nearest gl.NEAREST}
+                        filter-mode)]
+    (.texParameteri gl
+                    gl.TEXTURE_2D
+                    gl.TEXTURE_MIN_FILTER
+                    gl-filter-mode)
+    (.texParameteri gl
+                    gl.TEXTURE_2D
+                    gl.TEXTURE_MAG_FILTER
+                    gl-filter-mode))
+  (let [gl-wrap-mode ({:clamp gl.CLAMP_TO_EDGE
+                       :repeat gl.REPEAT}
+                      wrap-mode)]
+    (.texParameteri gl
+                    gl.TEXTURE_2D
+                    gl.TEXTURE_WRAP_S
+                    gl-wrap-mode)
+    (.texParameteri gl
+                    gl.TEXTURE_2D
+                    gl.TEXTURE_WRAP_T
+                    gl-wrap-mode)))
+
 (defn create-texture [gl
                       resolution
                       texture-type
@@ -34,28 +59,7 @@
                    format
                    webgl-type
                    nil))
-    (let [gl-filter-mode ({:linear gl.LINEAR
-                           :nearest gl.NEAREST}
-                          filter-mode)]
-      (.texParameteri gl
-                      gl.TEXTURE_2D
-                      gl.TEXTURE_MIN_FILTER
-                      gl-filter-mode)
-      (.texParameteri gl
-                      gl.TEXTURE_2D
-                      gl.TEXTURE_MAG_FILTER
-                      gl-filter-mode))
-    (let [gl-wrap-mode ({:clamp gl.CLAMP_TO_EDGE
-                         :repeat gl.REPEAT}
-                        wrap-mode)]
-      (.texParameteri gl
-                      gl.TEXTURE_2D
-                      gl.TEXTURE_WRAP_S
-                      gl-wrap-mode)
-      (.texParameteri gl
-                      gl.TEXTURE_2D
-                      gl.TEXTURE_WRAP_T
-                      gl-wrap-mode))
+    (set-texture-parameters gl tex filter-mode wrap-mode)
     tex))
 
 (defn create-f8-tex [gl resolution & [options]]
@@ -66,3 +70,20 @@
 
 (defn create-u32-tex [gl resolution & [options]]
   (create-texture gl resolution :u32 (merge options {:filter-mode :nearest})))
+
+(defn html-image-texture [gl img-id & [{:keys [wrap-mode
+                                               filter-mode]
+                                        :or {wrap-mode :repeat
+                                             filter-mode :linear}}]]
+  (let [texture (.createTexture gl)
+        image (.getElementById js/document img-id)]
+    (.bindTexture gl gl.TEXTURE_2D texture)
+    (set-texture-parameters gl texture filter-mode wrap-mode)
+    (.texImage2D gl
+                 gl.TEXTURE_2D
+                 0
+                 gl.RGBA
+                 gl.RGBA
+                 gl.UNSIGNED_BYTE
+                 image)
+    texture))
