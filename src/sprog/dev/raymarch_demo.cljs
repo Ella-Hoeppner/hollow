45834699671cf13211245e065a165a010bc76d4f
Ella Hoeppner <ellahoeppner@gmail.com>
2022-09-05 21:04:36 -0400

    Adding raymarching demo

---
+++ src/sprog/dev/raymarch_demo.cljs
@@ -0,0 +1,127 @@
+(ns sprog.dev.raymarch-demo
+  (:require [sprog.util :as u]
+            [sprog.webgl.canvas :refer [create-gl-canvas
+                                        square-maximize-gl-canvas]]
+            [sprog.webgl.shaders :refer [create-purefrag-sprog
+                                         run-purefrag-sprog]]
+            [clojure.walk :refer [postwalk-replace]]
+            [sprog.webgl.framebuffers :refer [target-screen!]]
+            [sprog.input.mouse :refer [mouse-pos
+                                       mouse-down?]]
+            [sprog.iglu.core :refer [iglu->glsl]]))
+
+(def start-time (u/now))
+
+(defonce gl-atom (atom nil))
+(defonce sprog-atom (atom nil))
+
+(def frag-source
+  (postwalk-replace
+   {:raymarch-step-factor "1.0"
+    :max-ray-dist "100.0"
+    :fov "0.5"
+    :distortion-amplitude-factor "0.1"
+    :distortion-frequency-factor "4.0"
+    :time-factor "0.25"
+    :TAU (.toFixed u/TAU 8)}
+   '{:version "300 es"
+     :precision {float highp
+                 usampler2D highp}
+     :uniforms {size float
+                time float
+                mouse vec2}
+     :outputs {fragColor vec4}
+     :signatures {distanceEstimate ([vec3] float)
+                  rayNormal ([vec3 vec3] vec3)
+                  main ([] void)}
+     :functions
+     {distanceEstimate
+      ([pos]
+       (+ (* :distortion-amplitude-factor
+             mouse.x
+             (+ (cos (* :TAU
+                        :distortion-frequency-factor
+                        (+ (* :time-factor time)
+                           pos.x)))
+                (sin (* :TAU
+                        :distortion-frequency-factor
+                        (+ (* :time-factor time)
+                           pos.y)))))
+          (- (length pos) "1.0")))
+      rayNormal
+      ([rayOrigin rayDirection]
+       (=float rayLength "0.0")
+       ("for(int i=0;i<512;i++)"
+        (=vec3 rayPos (+ rayOrigin (* rayDirection rayLength)))
+        (=float estimate (* :raymarch-step-factor (distanceEstimate rayPos)))
+        ("if" (|| (< (abs estimate) "0.00001")
+                  (> rayLength :max-ray-dist))
+              "break")
+        (+= rayLength estimate))
+       (=vec3 finalRayPos (+ rayOrigin (* rayLength rayDirection)))
+       (=vec2 e (vec2 "0.00025" 0))
+       (if (> rayLength :max-ray-dist)
+         (vec3 0)
+         (normalize (vec3 (- (distanceEstimate (+ finalRayPos e.xyy))
+                             (distanceEstimate (- finalRayPos e.xyy)))
+                          (- (distanceEstimate (+ finalRayPos e.yxy))
+                             (distanceEstimate (- finalRayPos e.yxy)))
+                          (- (distanceEstimate (+ finalRayPos e.yyx))
+                             (distanceEstimate (- finalRayPos e.yyx)))))))
+      main
+      ([]
+       (=vec2 pos (/ gl_FragCoord.xy size))
+
+       (=float h (tan (/ :fov "2.0")))
+       (=float viewportSize (* h "2.0"))
+
+       (=float focalLength "1.0")
+
+       (=vec3 cameraOrigin (vec3 0 0 -5))
+       (=vec3 cameraTarget (vec3 0 0 1))
+
+       (=vec3 vup (vec3 0 1 0))
+
+       (=vec3 w (normalize (- cameraOrigin cameraTarget)))
+       (=vec3 u (normalize (cross vup w)))
+       (=vec3 v (cross w u))
+
+       (=vec3 horizontal (* u h "2.0"))
+       (=vec3 vertical (* v h "2.0"))
+
+       (=vec3 lowerLeftCorner (- cameraOrigin
+                                 (+ (* "0.5" horizontal)
+                                    (* "0.5" vertical)
+                                    w)))
+
+       (=vec3 cameraDir (- (+ lowerLeftCorner
+                              (* pos.x horizontal)
+                              (* pos.y vertical))
+                           cameraOrigin))
+       (=vec3 surfaceNormal (rayNormal cameraOrigin cameraDir))
+
+       (= fragColor
+          (vec4 (* "0.5"
+                   (+ "1.0"
+                      surfaceNormal))
+                1)))}}))
+
+(defn update-page! []
+  (let [gl @gl-atom
+        resolution gl.canvas.width]
+    (square-maximize-gl-canvas gl)
+    (target-screen! gl)
+    (run-purefrag-sprog @sprog-atom
+                        resolution
+                        {:floats {"size" resolution
+                                  "time" (/ (- (u/now) start-time) 1000)
+                                  "mouse" (mouse-pos)}})
+    (js/requestAnimationFrame update-page!)))
+
+(defn init []
+  (let [gl (create-gl-canvas)]
+    (reset! gl-atom gl)
+    (reset! sprog-atom (create-purefrag-sprog
+                        gl
+                        (iglu->glsl frag-source))))
+  (update-page!))
--- src/sprog/dev/startup.cljs
+++ src/sprog/dev/startup.cljs
@@ -2,13 +2,15 @@
   (:require sprog.dev.basic-demo
             sprog.dev.multi-texture-output-demo
             sprog.dev.fn-sort-demo
-            sprog.dev.pixel-sort-demo))
+            sprog.dev.pixel-sort-demo
+            sprog.dev.raymarch-demo))
 
 (defn init []
   #_(sprog.dev.basic-demo/init)
-  (sprog.dev.multi-texture-output-demo/init)
+  #_(sprog.dev.multi-texture-output-demo/init)
   #_(sprog.dev.fn-sort-demo/init)
-  #_(sprog.dev.pixel-sort-demo/init))
+  #_(sprog.dev.pixel-sort-demo/init)
+  (sprog.dev.raymarch-demo/init))
 
 (defn pre-init []
   (js/window.addEventListener "load" (fn [_] (init))))
