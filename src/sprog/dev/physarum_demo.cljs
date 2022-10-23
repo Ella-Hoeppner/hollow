f887db9832cf87234df893b37d7bc8671e5a7d90
Ella Hoeppner <ellahoeppner@gmail.com>
2022-10-23 02:00:29 -0400

    simplifying and updating physarum demo

--- src/sprog/dev/physarum_demo.cljs
+++ src/sprog/dev/physarum_demo.cljs
@@ -10,7 +10,7 @@
             [sprog.iglu.chunks.misc :refer [offset-shortcut]]
             [sprog.iglu.chunks.particles :refer [particle-vert-source-u16
                                                  particle-frag-source-u16]]
-            [sprog.iglu.core :refer [merge-chunks]]))
+            [sprog.iglu.core :refer [iglu->glsl]]))
 
 (def substrate-resolution 1000)
 (def agent-tex-resolution 100)
@@ -35,19 +35,20 @@
 
 (defonce frame-atom (atom nil))
 
-(def bindings
-  {:uint16-max-f (.toFixed (dec (Math/pow 2 16)) 1)
-   :substrate-resolution-f (.toFixed substrate-resolution 1)
-   :agent-tex-resolution-f (.toFixed agent-tex-resolution 1)
-   :substrate-spread-factor (.toFixed substrate-spread-factor 8)
-   :substrate-fade-factor (.toFixed substrate-fade-factor 8)
-   :sensor-distance (.toFixed sensor-distance 8)
-   :sensor-spread (.toFixed sensor-spread 8)
-   :agent-speed-factor (.toFixed agent-speed-factor 8)
-   :agent-turn-factor (.toFixed agent-turn-factor 8)
-   :TAU (.toFixed u/TAU 8)})
-
-(def replace-bindings (partial postwalk-replace bindings))
+(def iglu-wrapper
+  (partial iglu->glsl
+           {:rand (fn [minimum maximum]
+                    (.toFixed (+ minimum (rand (- maximum minimum))) 12))
+            :uint16-max-f (.toFixed (dec (Math/pow 2 16)) 1)
+            :substrate-resolution-f (.toFixed substrate-resolution 1)
+            :agent-tex-resolution-f (.toFixed agent-tex-resolution 1)
+            :substrate-spread-factor (.toFixed substrate-spread-factor 8)
+            :substrate-fade-factor (.toFixed substrate-fade-factor 8)
+            :sensor-distance (.toFixed sensor-distance 8)
+            :sensor-spread (.toFixed sensor-spread 8)
+            :agent-speed-factor (.toFixed agent-speed-factor 8)
+            :agent-turn-factor (.toFixed agent-turn-factor 8)
+            :TAU (.toFixed u/TAU 8)}))
 
 (def substrate-sample-chunk
   '{:precision {usampler2D highp}
@@ -59,128 +60,125 @@
                     :uint16-max-f))}})
 
 (def render-frag-source
-  (replace-bindings
-   (merge-chunks
-    substrate-sample-chunk
-    '{:version "300 es"
-      :precision {float highp}
-      :uniforms {size vec2}
-      :outputs {fragColor vec4}
-      :signatures {main ([] void)}
-      :functions {main
-                  ([]
-                   (=float sampleValue
-                           (substrateSample (/ gl_FragCoord.xy size)))
-                   (= fragColor (vec4 sampleValue
-                                      sampleValue
-                                      sampleValue
-                                      1)))}})))
+  (iglu-wrapper
+   substrate-sample-chunk
+   '{:version "300 es"
+     :precision {float highp}
+     :uniforms {size vec2}
+     :outputs {fragColor vec4}
+     :signatures {main ([] void)}
+     :functions {main
+                 ([]
+                  (=float sampleValue
+                          (substrateSample (/ gl_FragCoord.xy size)))
+                  (= fragColor (vec4 sampleValue
+                                     sampleValue
+                                     sampleValue
+                                     1)))}}))
 
 (def substrate-logic-frag-source
-  (replace-bindings
-   (merge-chunks
-    substrate-sample-chunk
-    '{:version "300 es"
-      :precision {float highp}
-      :outputs {fragColor uvec4}
-      :signatures {main ([] void)}
-      :functions
-      {main
-       ([]
-        (=float centerSample (substrateSample (/ gl_FragCoord.xy
-                                                 :substrate-resolution-f)))
-        (=float averageNeightborSample
-                (/ (float
-                    (+ (substrateSample (/ (+ gl_FragCoord.xy (vec2 -1 -1))
-                                           :substrate-resolution-f))
-                       (substrateSample (/ (+ gl_FragCoord.xy (vec2 0 -1))
-                                           :substrate-resolution-f))
-                       (substrateSample (/ (+ gl_FragCoord.xy (vec2 1 -1))
-                                           :substrate-resolution-f))
-                       (substrateSample (/ (+ gl_FragCoord.xy (vec2 -1 0))
-                                           :substrate-resolution-f))
-                       (substrateSample (/ (+ gl_FragCoord.xy (vec2 1 0))
-                                           :substrate-resolution-f))
-                       (substrateSample (/ (+ gl_FragCoord.xy (vec2 -1 1))
-                                           :substrate-resolution-f))
-                       (substrateSample (/ (+ gl_FragCoord.xy (vec2 0 1))
-                                           :substrate-resolution-f))
-                       (substrateSample (/ (+ gl_FragCoord.xy (vec2 1 1))
-                                           :substrate-resolution-f))))
-                   "8.0"))
+  (iglu-wrapper
+   substrate-sample-chunk
+   '{:version "300 es"
+     :precision {float highp}
+     :outputs {fragColor uvec4}
+     :signatures {main ([] void)}
+     :functions
+     {main
+      ([]
+       (=float centerSample (substrateSample (/ gl_FragCoord.xy
+                                                :substrate-resolution-f)))
+       (=float averageNeightborSample
+               (/ (float
+                   (+ (substrateSample (/ (+ gl_FragCoord.xy (vec2 -1 -1))
+                                          :substrate-resolution-f))
+                      (substrateSample (/ (+ gl_FragCoord.xy (vec2 0 -1))
+                                          :substrate-resolution-f))
+                      (substrateSample (/ (+ gl_FragCoord.xy (vec2 1 -1))
+                                          :substrate-resolution-f))
+                      (substrateSample (/ (+ gl_FragCoord.xy (vec2 -1 0))
+                                          :substrate-resolution-f))
+                      (substrateSample (/ (+ gl_FragCoord.xy (vec2 1 0))
+                                          :substrate-resolution-f))
+                      (substrateSample (/ (+ gl_FragCoord.xy (vec2 -1 1))
+                                          :substrate-resolution-f))
+                      (substrateSample (/ (+ gl_FragCoord.xy (vec2 0 1))
+                                          :substrate-resolution-f))
+                      (substrateSample (/ (+ gl_FragCoord.xy (vec2 1 1))
+                                          :substrate-resolution-f))))
+                  "8.0"))
 
-        (= fragColor
-           (uvec4 (* (* (mix centerSample
-                             averageNeightborSample
-                             :substrate-spread-factor)
-                        (- "1.0" :substrate-fade-factor))
-                     :uint16-max-f)
-                  0
-                  0
-                  0)))}})))
+       (= fragColor
+          (uvec4 (* (* (mix centerSample
+                            averageNeightborSample
+                            :substrate-spread-factor)
+                       (- "1.0" :substrate-fade-factor))
+                    :uint16-max-f)
+                 0
+                 0
+                 0)))}}))
 
 (def agent-logic-frag-source
-  (offset-shortcut
-   (replace-bindings
-    (merge-chunks
-     rand-chunk
-     substrate-sample-chunk
-     '{:version "300 es"
-       :precision {float highp}
-       :uniforms {agentTex usampler2D
-                  randomizeChance float
-                  time float}
-       :outputs {fragColor uvec4}
-       :signatures {main ([] void)}
-       :functions
-       {main
-        ([]
-         (=vec2 pos (/ gl_FragCoord.xy :agent-tex-resolution-f))
-         (=uvec4 oldFragColor (texture agentTex pos))
+  (iglu-wrapper
+   rand-chunk
+   substrate-sample-chunk
+   '{:version "300 es"
+     :precision {float highp}
+     :uniforms {agentTex usampler2D
+                randomizeChance float
+                time float}
+     :outputs {fragColor uvec4}
+     :signatures {main ([] void)}
+     :functions
+     {main
+      ([]
+       (=vec2 pos (/ gl_FragCoord.xy :agent-tex-resolution-f))
+       (=uvec4 oldFragColor (texture agentTex pos))
 
-         (=vec2 agentPos (/ (vec2 oldFragColor.xy) :uint16-max-f))
-         (=float agentAngle (* :TAU (/ (float oldFragColor.z) :uint16-max-f)))
+       (=vec2 agentPos (/ (vec2 oldFragColor.xy) :uint16-max-f))
+       (=float agentAngle (* :TAU (/ (float oldFragColor.z) :uint16-max-f)))
 
-         (=float clockwiseAngle (+ agentAngle :sensor-spread))
-         (=float clockwiseSensorSample
-                 (substrateSample
-                  (+ agentPos
-                     (* :sensor-distance
-                        (vec2 (cos clockwiseAngle)
-                              (sin clockwiseAngle))))))
+       (=float clockwiseAngle (+ agentAngle :sensor-spread))
+       (=float clockwiseSensorSample
+               (substrateSample
+                (+ agentPos
+                   (* :sensor-distance
+                      (vec2 (cos clockwiseAngle)
+                            (sin clockwiseAngle))))))
 
-         (=float counterclockwiseAngle (- agentAngle :sensor-spread))
-         (=float counterclockwiseSensorSample
-                 (substrateSample
-                  (+ agentPos
-                     (* :sensor-distance
-                        (vec2 (cos counterclockwiseAngle)
-                              (sin counterclockwiseAngle))))))
+       (=float counterclockwiseAngle (- agentAngle :sensor-spread))
+       (=float counterclockwiseSensorSample
+               (substrateSample
+                (+ agentPos
+                   (* :sensor-distance
+                      (vec2 (cos counterclockwiseAngle)
+                            (sin counterclockwiseAngle))))))
 
-         (=float newAgentAngle
-                 (mod (+ agentAngle
-                         (* :agent-turn-factor
-                            (- clockwiseSensorSample
-                               counterclockwiseSensorSample)))
-                      :TAU))
-         (=vec2 newAgentPos
-                (mod (+ agentPos
-                        (* :agent-speed-factor
-                           (vec2 (cos newAgentAngle)
-                                 (sin newAgentAngle))))
-                     "1.0"))
+       (=float newAgentAngle
+               (mod (+ agentAngle
+                       (* :agent-turn-factor
+                          (- clockwiseSensorSample
+                             counterclockwiseSensorSample)))
+                    :TAU))
+       (=vec2 newAgentPos
+              (mod (+ agentPos
+                      (* :agent-speed-factor
+                         (vec2 (cos newAgentAngle)
+                               (sin newAgentAngle))))
+                   "1.0"))
 
-         (=vec2 randSeed (* "400.0" (+ pos (vec2 (mod time "3.217") 0))))
+       (=vec2 randSeed (* "400.0" (+ pos (vec2 (mod time "3.217") 0))))
 
-         (= fragColor
-            (uvec4 (* (if (< (rand [:offset randSeed]) randomizeChance)
-                        (vec3 (rand [:offset randSeed])
-                              (rand [:offset randSeed])
-                              (rand [:offset randSeed]))
-                        (vec3 newAgentPos
-                              (/ newAgentAngle :TAU)))
-                      :uint16-max-f)
-                   0)))}}))))
+       (= fragColor
+          (uvec4 (* (if (< (rand (+ randSeed [:rand -100 100]))
+                           randomizeChance)
+                      (vec3 (rand (+ randSeed [:rand -100 100]))
+                            (rand (+ randSeed [:rand -100 100]))
+                            (rand (+ randSeed [:rand -100 100])))
+                      (vec3 newAgentPos
+                            (/ newAgentAngle :TAU)))
+                    :uint16-max-f)
+                 0)))}}))
 
 (defn update-agents! [randomize-chance]
   (let [gl @gl-atom
@@ -223,12 +221,12 @@
   (let [gl @gl-atom
         resolution [gl.canvas.width gl.canvas.height]]
     (square-maximize-gl-canvas gl)
-    (run-purefrag-autosprog
-     gl
-     render-frag-source
-     resolution
-     {:floats {"size" resolution}
-      :textures {"substrate" (first @substrate-texs-atom)}}))
+    (run-purefrag-autosprog gl
+                            render-frag-source
+                            resolution
+                            {:floats {"size" resolution}
+                             :textures {"substrate" (first 
+                                                     @substrate-texs-atom)}}))
   (swap! frame-atom inc)
   (js/requestAnimationFrame update-page!))
 
