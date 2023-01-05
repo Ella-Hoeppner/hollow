50e15d33ca30da8dada0034ddee26aa222f5e489
Ella Hoeppner <ellahoeppner@gmail.com>
2023-01-04 23:38:59 -0500

    adding functions for modifying sprog state

--- src/sprog/webgl/core.cljs
+++ src/sprog/webgl/core.cljs
@@ -1,5 +1,6 @@
 (ns sprog.webgl.core
-  (:require [sprog.dom.canvas :refer [create-gl-canvas]]))
+  (:require [sprog.util :as u]
+            [sprog.dom.canvas :refer [create-gl-canvas]]))
 
 (defonce sprogs-atom (atom nil))
 
@@ -28,6 +29,36 @@
 
 (defn sprog-state [& sprog-name]
   (:state (@sprogs-atom (or sprog-name :default))))
+
+(defn set-sprog-state!
+  ([sprog-name new-state]
+   (swap! sprogs-atom
+          update
+          sprog-name
+          update
+          :state
+          (fn [state]
+            (reset! state new-state)
+            state))
+   new-state)
+  ([new-state]
+   (set-sprog-state! :default new-state)))
+
+(defn update-sprog-state! [& args]
+  (let [[sprog-name update-fn update-fn-args]
+        (if (keyword? (first args))
+          [(first args) (second args) (drop 2 args)]
+          [:default (first args) (rest args)])]
+    (swap! sprogs-atom
+           update
+           sprog-name
+           update
+           :state
+           (fn [state]
+             (apply (partial swap! state update-fn)
+                    update-fn-args)
+             state))
+    @(get-in @sprogs-atom [sprog-name :state])))
 
 (defn sprog-context [& sprog-name]
   (:gl (@sprogs-atom (or sprog-name :default))))
