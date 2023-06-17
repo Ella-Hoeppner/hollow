0b55368f130c2b8987964dfc717f0345455f2151
Ella Hoeppner <ellahoeppner@gmail.com>
2023-06-17 00:26:04 -0400

    changing array demo to show array initialization

--- src/sprog/dev/array_demo.cljs
+++ src/sprog/dev/array_demo.cljs
@@ -14,18 +14,21 @@
     '{:version "300 es"
       :precision {float highp}
       :uniforms {size vec2
-                 color-vectors [vec3 2]
-                 decisions bvec3}
+                 decisions [bool 3]}
       :outputs {fragColor vec4}
       :functions
-      {decide-colors {([[vec3 2] bvec3] vec3)
+      {decide-colors {([[vec3 2] [bool 3]] vec3)
                       ([c d]
                        (vec3
-                        (.r (if (== d.x "true") [c 0] [c 1]))
-                        (.g (if (== d.y "true") [c 0] [c 1]))
-                        (.b (if (== d.z "true") [c 0] [c 1]))))}}
+                        (.r (if [d "0"] [c "0"] [c "1"]))
+                        (.g (if [d "1"] [c "0"] [c "1"]))
+                        (.b (if [d "2"] [c "0"] [c "1"]))))}}
       :main
-      ((= fragColor
+      ((= [vec3 "2"]
+          color-vectors
+          [vec3 2 [(vec3 1 1 1)
+                   (vec3 0 0 0)]])
+       (= fragColor
           (vec4 (decide-colors color-vectors decisions)
                 1)))})))
 
@@ -36,8 +39,6 @@
      frag-glsl
      (canvas-resolution)
      {"size" (canvas-resolution)
-      "color-vectors" [1 1 1
-                       0 0 0]
       "decisions" (mapv #(if (> (Math/sin (* u/TAU
                                              (+ (u/seconds-since-startup) %)))
                                 0)
