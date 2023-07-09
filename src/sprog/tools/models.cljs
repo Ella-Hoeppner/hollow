a81105a9fe49431d3d9c2bdfaa6bdf7fa7b80346
Fay Carsons <faycarsons23@gmail.com>
2023-07-09 12:12:02 -0400

    added triangle partitioning to obj-parser

--- src/sprog/tools/models.cljs
+++ src/sprog/tools/models.cljs
@@ -1,41 +1,57 @@
 (ns sprog.tools.models
   (:require [sprog.util :as u]
-            [clojure.string :as str]
-            [clojure.walk :refer [keywordize-keys]]))
+            [clojure.string :as s]))
 
-;parses obj files, fairly barebones currently. 
-;partitioning must be handled post-parsing, no support for groups,
-;doesn't re-arrange by face-indices
-(defn parse-model [file]
-  (let [lines (str/split file "\n")]
-    (update
-     (into {}
-           (map (fn [[k v]]
-                  [k (vec (flatten v))])
-                (reduce
-                 (fn [parsed line]
-                   (let [tokens (remove empty?
-                                        (map #(str/replace % #"(\s|/|\n)" "")
-                                             (str/split line #"(\s|/|\n)")))
-                         data-type (first tokens)
-                         data (rest tokens)
-                         parse-line #(conj %
-                                           (mapv parse-double data))]
-                     (case data-type
-                       "v" (update parsed :positions parse-line)
-                       "vt" (update parsed :texcoords parse-line)
-                       "vn" (update parsed :normals parse-line)
-                       "vp" (update parsed :param-space parse-line)
-                       "f" (update parsed :indices parse-line)
-                       "l" (update parsed :lines parse-line)
-                       parsed)))
-                 {:positions []
-                  :texcoords []
-                  :normals []
-                  :param-space []
-                  :indices []
-                  :lines []}
-                 lines)))
-     :indices
-     (comp #(mapv vec %)
-           (partial partition 3)))))
+(defn parse-face [face]
+  (vec
+   (flatten (map parse-long
+                 (s/split face "/")))))
+
+
+(defn partition-triangles [quad]
+  (let [order (flatten (partition 3 1 (range 4)))]
+    (mapv (fn [q]
+            (mapv (fn [i]
+                    (nth q i))
+                  order))
+          quad)))
+
+(defn parse-obj
+  "parses obj files, fairly barebones. 
+   only supports triangles and quads, 
+   assumes same # of vertices/normals/texcoords"
+  [file]
+  (let [str->param {"v" :positions
+                    "vt" :texcoords
+                    "vn" :normals
+                    "vp" :param-space
+                    "f" :indices
+                    "l" :lines}
+        lines (s/split file "\n")
+        parsed (reduce
+                (fn [acc line]
+                  (let [tokens (remove #(or (empty? %)
+                                            (re-matches #"(\s|\n)" %))
+                                       (s/split line #"(\s|\n)"))
+                        [data-type & data] tokens
+                        face? (= data-type "f")
+                        quads? (and face?
+                                    (= (count data) 4))
+                        parse-line #(conj % (mapv (if face?
+                                                    parse-face
+                                                    parse-double)
+                                                  data))]
+
+                    (-> acc
+                        (update (str->param data-type) parse-line)
+                        (assoc :quads? quads?))))
+                {:positions []
+                 :texcoords []
+                 :normals []
+                 :param-space []
+                 :indices []
+                 :lines []}
+                lines)]
+    (if (:quads? parsed)
+      (update parsed :indices partition-triangles)
+      parsed)))
