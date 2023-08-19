f410babca6bbb784728ae055a763f03af9a04f6f
Ella Hoeppner <ellahoeppner@gmail.com>
2023-08-18 20:44:44 -0400

    re-implementing midi input handling and demo

--- src/hollow/demos/midi.cljs
+++ src/hollow/demos/midi.cljs
@@ -1,96 +1,58 @@
 (ns hollow.demos.midi
   (:require
    [hollow.util :as u]
-   [hollow.input.midi :refer [add-midi-callback]]
+   [hollow.input.midi :refer [add-note-down-callback
+                              add-note-up-callback
+                              down-notes]]
    [kudzu.core :refer [kudzu->glsl]]
    [hollow.dom.canvas :refer [maximize-gl-canvas
                               canvas-resolution]]
    [hollow.webgl.shaders :refer [run-purefrag-shader!]]
-   [kudzu.chunks.misc :refer [pos-chunk
-                              rescale-chunk]]
-   [kudzu.chunks.noise :refer [simplex-3d-chunk]]
+   [kudzu.chunks.misc :refer [pos-chunk]]
+   [kudzu.tools :refer [unquotable]]
    [hollow.webgl.core
     :refer-macros [with-context]
-    :refer [start-hollow!]]))
+    :refer [start-hollow!
+            update-hollow-state!]]))
 
-(def smooth-factor 0.2)
+(def circle-count 64)
 
-(defonce circle-radius-atom (atom 0))
-(defonce circle-radius-target-atom (atom 1))
-(defonce noise-scale-atom (atom 0))
-(defonce noise-scale-target-atom (atom 1))
-
-; midi event callback
-(defn midi-event-handler [{:keys [note velocity]}]
-  (reset! circle-radius-target-atom (/ note 127))
-  (reset! noise-scale-target-atom (/ velocity 127)))
-
-; shader source
 (def frag-glsl
-  (kudzu->glsl
-   pos-chunk
-   rescale-chunk
-   simplex-3d-chunk
-   '{:precision {float highp}
-     :uniforms {size vec2
-                time float
-                circleRadius float
-                noiseScale float}
-     :outputs {fragColor vec4}
-     :main ((=vec2 pos (getPos))
+  (unquotable
+   (kudzu->glsl
+    pos-chunk
+    '{:precision {float highp}
+      :uniforms {size vec2
+                 time float
+                 circles [vec2 ~(str circle-count)]}
+      :outputs {frag-color vec4}
+      :main ((=vec2 pos (getPos))
+             (= frag-color (vec4 0 0 0 1))
+             (:for [i ~circle-count]
+                   (:if (< (distance pos [circles i]) 0.1)
+                        (= frag-color (vec4 1)))))})))
 
-            ; determine if current pixel is inside or outside noisey circle
-            (=float circleValue
-                    (->> pos
-                         ; find signed distance to circle at center of screen
-                         (- (vec2 0.5))
-                         length
-                         (- circleRadius)
-                         ; add offset to distance with noise function
-                         (+ (* noiseScale (snoise3D (vec3 (* 10 pos) time))))
-                         ; separate inside and outside with smoothstep
-                         (smoothstep 0 0.01)))
-
-            ; define colors
-            (=vec3 background (mix (vec3 0.529 0.807 0.921) (vec3 1) pos.y))
-            (=vec3 circleInterior (vec3 0.760 0.698 0.501))
-
-            ; tween background and circle color
-            (=vec3 col (mix background circleInterior circleValue))
-
-            ; output to pixel 
-            (= fragColor (vec4 col 1)))}))
-
-(defn render [gl]
+(defn update-page! [{:keys [gl circles] :as state}]
   (with-context gl
-    ; stretch canvas to fit window
     (maximize-gl-canvas)
-    ; run shader, at canvas resolution, with uniforms in map
-    (run-purefrag-shader!
-     frag-glsl
-     (canvas-resolution)
-     {"size" (canvas-resolution)
-      "time" (u/seconds-since-startup)
-      "circleRadius" (u/scale 0.05 0.3 @circle-radius-atom)
-      "noiseScale" (u/scale 0 0.05 @noise-scale-atom)})))
-
-(defn update-states! []
-  (swap! circle-radius-atom
-         #(u/scale % @circle-radius-target-atom smooth-factor))
-  (swap! noise-scale-atom
-         #(u/scale % @noise-scale-target-atom smooth-factor)))
-
-(defn update-page! [{:keys [gl]}]
-  (render gl)
-  (update-states!)
-  {})
+    (run-purefrag-shader! frag-glsl
+                          (canvas-resolution)
+                          {"size" (canvas-resolution)
+                           "circles" (flatten
+                                      (take circle-count
+                                            (concat (vals circles)
+                                                    (repeat [-10 -10]))))
+                           "time" (u/seconds-since-startup)}))
+  state)
 
 (defn init []
   (js/window.addEventListener
    "load"
    (fn []
-     ; initialize midi and register midi event callback
-     (add-midi-callback midi-event-handler)
-
-     ; start update loop
+     (add-note-down-callback
+      (fn [{:keys [note]}]
+        (update-hollow-state! #(update % :circles assoc note [(rand) (rand)]))))
+     (add-note-up-callback
+      (fn [{:keys [note]}]
+        (update-hollow-state! #(update % :circles dissoc note))))
      (start-hollow! nil update-page!))))
--- src/hollow/input/midi.cljs
+++ src/hollow/input/midi.cljs
@@ -1,20 +1,74 @@
 (ns hollow.input.midi
-  (:require [hollow.util :as u]))
+  (:require [hollow.util :as u]
+            [clojure.set :refer [union]]))
 
-(defn recieve-midi-message [callback message]
-  (let [[command-and-channel note velocity] (seq message.data)]
-    (callback {:command (bit-shift-right command-and-channel 4)
-               :channel (bit-and command-and-channel 0xf)
-               :note note
-               :velocity velocity})))
+(defonce midi-callacks-atom (atom {}))
 
-(defn on-midi-success [callback midi-access]
-  (doseq [[_ input] midi-access.inputs]
-    (set! input.onmidimessage (partial recieve-midi-message callback))))
+(defonce down-keys-atom (atom {}))
 
-(defn add-midi-callback
+(def command-map
+  {9 :note-down
+   8 :note-up})
+
+(defn on-midi-event [message]
+  (let [[command-and-channel & data] (seq message.data)
+        command (bit-shift-right command-and-channel 4)
+        channel (bit-and command-and-channel 0xf)
+        callbacks @midi-callacks-atom]
+    (when-let [generic-callback (callbacks :generic)]
+      (generic-callback {:command command
+                         :channel channel
+                         :data data}))
+    (let [command-type (command-map command)]
+      (when-let [command-callback (callbacks command-type)]
+        (command-callback 
+         (cond
+           (= :note-down command-type)
+           (assoc (zipmap [:note :velocity] data)
+                  :channel
+                  channel)
+
+           (= :note-up command-type)
+           {:note (first data)
+            :channel channel})))
+      (when (= command-type :note-down)
+        (swap! down-keys-atom
+               update
+               channel
+               (comp set conj)
+               (first data)))
+      (when (= command-type :note-up)
+        (swap! down-keys-atom
+               update
+               channel
+               disj
+               (first data))))))
+
+(defn add-generic-midi-callback
   "`callback`: a function that accepts a map containing the keys :command,
-   :channel, :note, and :velocity."
+   :channel, and :data (the latter 2 midi data bytes)."
   [callback]
-  (.then (.requestMIDIAccess js/navigator)
-         (partial on-midi-success callback)))
+  (swap! midi-callacks-atom assoc :generic callback))
+
+(defn add-note-down-callback
+  "`callback`: a function that accepts a map containing the keys :channel,
+   :note, and :velocity."
+  [callback]
+  (swap! midi-callacks-atom assoc :note-down callback))
+
+(defn add-note-up-callback
+  "`callback`: a function that accepts a map containing the keys :channel and
+   :note."
+  [callback]
+  (swap! midi-callacks-atom assoc :note-up callback))
+
+(defn down-notes
+  ([channel]
+   (set (@down-keys-atom channel)))
+  ([]
+   (apply union (vals @down-keys-atom))))
+
+(.then (.requestMIDIAccess js/navigator)
+       (fn [midi-access]
+         (doseq [[_ input] midi-access.inputs]
+           (set! input.onmidimessage on-midi-event))))
