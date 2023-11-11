843840687cbba23b92791c32a9a4fbaad28fee22
Ella Hoeppner <ellahoeppner@gmail.com>
2023-11-11 14:41:45 -0500

    updating kuduz, reworking okhsl example

--- src/hollow/demos/okhsl.cljs
+++ src/hollow/demos/okhsl.cljs
@@ -3,7 +3,9 @@
                                        canvas-resolution]]
             [hollow.webgl.shaders :refer [run-purefrag-shader!]]
             [kudzu.core :refer [kudzu->glsl]]
-            [kudzu.chunks.color.oklab :refer [okhsl-chunk]]
+            [kudzu.chunks.color.oklab :refer [okhsl-chunk
+                                              okhsv-chunk]]
+            [kudzu.chunks.color.rgb :refer [lrgb-chunk]]
             [hollow.webgl.core
              :refer-macros [with-context]
              :refer [start-hollow!]]
@@ -12,12 +14,18 @@
 (def frag-source
   (kudzu->glsl
    okhsl-chunk
+   okhsv-chunk
+   lrgb-chunk
    '{:precision {float highp}
      :uniforms {resolution vec2
                 hue float}
      :outputs {frag-color vec4}
      :main ((=vec2 pos (pixel-pos :uni))
-            (= frag-color (vec4 (okhsl->srgb (vec3 hue pos)) 1)))}))
+            (= frag-color
+               (-> (vec3 hue pos)
+                   okhsl->lrgb
+                   lrgb->srgb
+                   (vec4 1))))}))
 
 (defn update-page! [{:keys [gl] :as state}]
   (with-context gl
