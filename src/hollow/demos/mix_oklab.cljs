700084d715e33cc680acad7430ac63f4ac124136
Ella Hoeppner <ellahoeppner@gmail.com>
2023-12-17 14:39:04 -0500

    updating kudzu, tweaking mix-oklab demo

--- src/hollow/demos/mix_oklab.cljs
+++ src/hollow/demos/mix_oklab.cljs
@@ -1,7 +1,8 @@
-(ns hollow.demos.oklab-mix
+(ns hollow.demos.mix-oklab
   (:require [hollow.dom.canvas :refer [maximize-gl-canvas
                                        canvas-resolution]]
             [hollow.webgl.shaders :refer [run-purefrag-shader!]]
+            [kudzu.chunks.color.rgb :refer [lrgb-chunk]]
             [kudzu.chunks.color.oklab :refer [mix-oklab-chunk]]
             [kudzu.core :refer [kudzu->glsl]]
             [hollow.webgl.core
@@ -10,20 +11,29 @@
 
 (def frag-source
   (kudzu->glsl
+   lrgb-chunk
    mix-oklab-chunk
    '{:precision {float highp}
      :uniforms {size vec2}
      :outputs {frag-color vec4}
      :main ((=vec2 pos (/ gl_FragCoord.xy size))
             (= frag-color
-               (if (> pos.y (/ 2 3))
-                 (vec4 (mix (vec3 0 0 1) (vec3 1) pos.x)
-                       1)
-                 (if (> pos.y (/ 1 3))
-                   (vec4 (mix-oklab (vec3 0 0 1) (vec3 1) pos.x)
-                         1)
-                   (vec4 (mix-oklab (vec3 0 0 1) (vec3 1) pos.x 0.2)
-                         1)))))}))
+               (-> (if (> pos.y (/ 2 3))
+                     ; simple lrgb mixing
+                     (mix (vec3 0 0 1) (vec3 1) pos.x)
+                     (if (> pos.y (/ 1 3))
+                       ; oklab mixing
+                       (mix-oklab (vec3 0 0 1) (vec3 1) pos.x)
+                       ; oklab mixing with a boosted middle
+                       (oklab->lrgb
+                        (* (+ 1 (* 0.2
+                                   pos.x
+                                   (- 1 pos.x)))
+                           (mix (lrgb->oklab (vec3 0 0 1))
+                                (lrgb->oklab (vec3 1))
+                                pos.x)))))
+                   lrgb->srgb
+                   (vec4 1))))}))
 
 (defn init-page! [gl]
   (with-context gl
